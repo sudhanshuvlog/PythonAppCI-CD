42100df3d401f6ad1f24e3ad95eefe9c8075cae1
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2024-05-12 16:10:03 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Wow CI-CD Pipeline is been developed..'
+    expected_text = 'Hello World!'
     assert expected_text.encode() in response.data
