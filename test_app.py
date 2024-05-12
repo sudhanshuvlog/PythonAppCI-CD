79f988b7e759e9ac5e8e347fee25b52f89ad5d98
sudhanshuunilever <sudhanshu.pandey@unilever.com>
2024-05-12 19:02:34 +0530

    first changes

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Hello World!'
+    expected_text = 'Hello World123!'
     assert expected_text.encode() in response.data
